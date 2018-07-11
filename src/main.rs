333320eb427570b10aca9d9f70f33d0e41b3e7a9
Ismael González <ismgonval@gmail.com>
2018-07-12 00:44:52 +0200

    Update crate documentation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,40 @@
 //! # RnR
 //!
 //! `rnr` is a command-line tool to batch rename files for ANSI terminals.
+//!
+//! # Options
+//! ```sh
+//! USAGE:
+//!    rnr [FLAGS] [OPTIONS] <EXPRESSION> <REPLACEMENT> <FILE(S)>...
+//!
+//! FLAGS:
+//!    -b, --backup     Generate file backups before renaming
+//!    -n, --dry-run    Only show what would be done (default mode)
+//!    -f, --force      Make actual changes to files
+//!    -h, --help       Prints help information
+//!    -V, --version    Prints version information
+//!
+//! OPTIONS:
+//!    -d, --max-depth <LEVEL>    Set max depth in recursive mode
+//!    -r, --recursive <PATH>     Recursive mode
+//!
+//! ARGS:
+//!    <EXPRESSION>     Expression to match (can be a regex)
+//!    <REPLACEMENT>    Expression replacement
+//!    <FILE(S)>...     Target files
+//!
+//! ```
+//! ## Default behavior
+//! * *Dry-run* by default.
+//! * Only **UTF-8 valid** input arguments and filenames.
+//! * Works on files and symlinks (ignores directories).
+//! * Accepts multiple files as arguments.
+//! * Accepts a **regex** to generate matches. These expressions have same
+//!   limitations of `regex` crate.
+//! * If max depth is not provided to recursive mode, it is assumed *infite*.
+//! * Does not generate backups.
+//! * Output is *always colored*. [TODO: *no color mode / silent mode*]
+//!
 extern crate ansi_term;
 extern crate clap;
 extern crate regex;
