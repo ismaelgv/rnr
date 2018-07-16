4b350ffcc7e66150c5339df5d18dd863704df783
Ismael González <ismgonval@gmail.com>
2018-07-16 14:30:40 +0200

    New crate release: v0.1.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,8 @@
-# UNRELEASED
+# v0.1.2 (2018-08-16)
 ### Added
 * Silent mode.
 * Option to set color mode (always, auto, never).
+* New tests.
 
 ### Changed
 * New modules and heavy code reorganization: `output` and `fileutils`
--- src/app.rs
+++ src/app.rs
@@ -100,7 +100,7 @@ fn parse_arguments() -> Config {
 /// Configure application using clap. It sets all options and command-line help.
 fn config_app<'a>() -> App<'a, 'a> {
     App::new("rnr")
-        .version("0.1.1")
+        .version("0.1.2")
         .author("Ismael González <ismgonval@gmail.com>")
         .about("\nrnr is simple file renamer written in Rust.")
         .arg(
