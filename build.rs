cbc4745ca595cea4ce9d68cc371de31cbd94db94
Ismael González <ismgonval@gmail.com>
2025-01-11 15:13:29 +0100

    feat: generate man pages

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
-use clap::CommandFactory;
+use clap::{Command, CommandFactory};
 use clap_complete::{generate_to, Shell};
-use std::io::Error;
+use std::{io::Error, path::PathBuf};
 
 include!("src/cli.rs");
 
@@ -19,14 +19,24 @@ fn main() -> Result<(), Error> {
     }
 
     // Man
+    for subcommand in cmd.get_subcommands() {
+        let name = format!("{}-{}", APP_NAME, subcommand.get_name());
+        generate_man(subcommand.clone(), &outdir, &name)?;
+    }
+    generate_man(cmd, &outdir, APP_NAME)?;
+
+    Ok(())
+}
+
+fn generate_man(cmd: Command, outdir: &PathBuf, name: &str) -> Result<(), Error> {
     let man = clap_mangen::Man::new(cmd);
     let mut buffer: Vec<u8> = Default::default();
     man.render(&mut buffer)?;
 
-    let mut man_file = outdir.join(APP_NAME);
+    let mut man_file = outdir.join(name);
     man_file.set_extension("1");
     std::fs::write(man_file, buffer)?;
-    println!("cargo:warning=man file is generated.");
+    println!("cargo:warning={name} man file is generated.");
 
     Ok(())
 }
